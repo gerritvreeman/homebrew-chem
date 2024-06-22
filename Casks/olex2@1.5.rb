6ea2a4374c45d73fdae029eaac4d6f859708408f
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-22 18:57:33 -0500

    Update and rename olex2-1.5.rb to olex2@1.5.rb

--- Casks/olex2@1.5.rb
+++ Casks/olex2@1.5.rb
@@ -1,4 +1,4 @@
-cask "olex2-1.5" do
+cask "olex2@1.5" do
   version "1.5"
   sha256 "40ee824f3acd4fc258328d196f8ca35f3e0c0eb5168d86b0c188f788242a8470"
   url "https://secure.olex2.org/olex2-distro/olex2-#{version}.app.dmg",
