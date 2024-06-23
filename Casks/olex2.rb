bbc6e00f7d6764daeba11a78d3bdf27b4c75cbad
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-22 23:05:35 -0500

    Update and rename olex2@1.5.rb to olex2.rb

--- Casks/olex2.rb
+++ Casks/olex2.rb
@@ -1,9 +1,9 @@
-cask "olex2@1.5" do
+cask "olex2" do
   version "1.5"
   sha256 "40ee824f3acd4fc258328d196f8ca35f3e0c0eb5168d86b0c188f788242a8470"
   url "https://secure.olex2.org/olex2-distro/olex2-#{version}.app.dmg",
       verified: "secure.olex2.org/"
-  name "olex2-1.5"
+  name "olex2"
   desc " An easy-to-use program containing everything you need to solve, refine and finish small-molecule crystal structures."
   homepage "https://www.olexsys.org/olex2/"
   app "olex2.app"
