bd38793b30c96f5c00fe97eb4449ddd116ff5268
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-22 23:42:49 -0500

    Update olex2.rb

--- Casks/olex2.rb
+++ Casks/olex2.rb
@@ -1,12 +1,9 @@
 cask "olex2" do
   version "1.5"
   sha256 "40ee824f3acd4fc258328d196f8ca35f3e0c0eb5168d86b0c188f788242a8470"
-  #url "https://secure.olex2.org/olex2-distro/olex2-#{version}.app.dmg",
-  url "https://secure.olex2.org/olex2-distro/#{version}/olex2-mac64-intel.zip",
+  url "https://secure.olex2.org/olex2-distro/olex2-#{version}.app.dmg",
       verified: "secure.olex2.org/",
-      header: [
-        "Last-Modified: Sun, 23 Jun 2024 04:22:40 GMT"
-        ]
+      header: "Last-Modified: Sun, 23 Jun 2024 04:22:40 GMT"
   name "olex2"
   desc " An easy-to-use program containing everything you need to solve, refine and finish small-molecule crystal structures."
   homepage "https://www.olexsys.org/olex2/"
