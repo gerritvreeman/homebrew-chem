637f19de2f516ec1a7a414c34f2023dc054a28eb
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-23 01:04:34 -0500

    Update olex2.rb

--- Casks/olex2.rb
+++ Casks/olex2.rb
@@ -1,7 +1,6 @@
 cask "olex2" do
-  version "1.3"
-  sha256 :no_check
-  #sha256 "40ee824f3acd4fc258328d196f8ca35f3e0c0eb5168d86b0c188f788242a8470"
+  version "1.5"
+  sha256 "40ee824f3acd4fc258328d196f8ca35f3e0c0eb5168d86b0c188f788242a8470"
   url "https://secure.olex2.org/olex2-distro/olex2-#{version}.app.dmg",
       verified: "secure.olex2.org/"
   name "olex2"
