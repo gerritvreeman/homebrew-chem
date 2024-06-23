44cab9ec34d0e676756266cfc6dd0d591734d2c9
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-22 23:25:58 -0500

    Update crystalexplorer.rb
    added verified

--- Casks/crystalexplorer.rb
+++ Casks/crystalexplorer.rb
@@ -2,7 +2,8 @@ cask "crystalexplorer" do
   version "21.3"
   sha256 "cb74e3254258fc7f36b5e32e4923eb3d072c93a1a7f4bbfd812df70e79314042"
 
-  url "https://releases.crystalexplorer.net/CrystalExplorer-#{version}-Darwin.dmg"
+  url "https://releases.crystalexplorer.net/CrystalExplorer-#{version}-Darwin.dmg",
+      verified: "releases.crystalexplorer.net/"
   name "CrystalExplorer-21"
   desc "Hirshfeld surfaces, interaction energies and more..."
   homepage "https://crystalexplorer.scb.uwa.edu.au/"
