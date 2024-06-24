9d7fa438450e2a1dc1f6c3dd21695af5e643ed65
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-24 08:52:47 -0500

    Add files via upload

---
+++ Casks/mnova.rb
@@ -0,0 +1,9 @@
+cask "mnova" do
+  version "15.0.1"
+  sha256 "00000"
+  url "https://mestrelab.com/downloads/mnova/mac/MestReNova-#{version}-35756.dmg"
+  name "mnova"
+  desc " An easy-to-use program containing everything you need to solve, refine and finish small-molecule crystal structures."
+  homepage "https://mestrelab.com"
+  app "MestReNova.app"
+end
