8f595f35d5e5e7162ebbf2a5875f36ecc6ab390c
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-24 08:56:57 -0500

    Update mnova.rb

--- Casks/mnova.rb
+++ Casks/mnova.rb
@@ -1,6 +1,6 @@
 cask "mnova" do
   version "15.0.1"
-  sha256 "00000"
+  sha256 "f3d5c7638f2bcb6b60d83be4a85ca7fa4a2773686017b06ea45c1dbc7d99c497"
   url "https://mestrelab.com/downloads/mnova/mac/MestReNova-#{version}-35756.dmg"
   name "mnova"
   desc " An easy-to-use program containing everything you need to solve, refine and finish small-molecule crystal structures."
