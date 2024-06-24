87a4e4cd84b1b6ec37553013c6e5b2021054a9d2
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-24 09:59:39 -0500

    Create colorpicker-solarized.rb

---
+++ Casks/colorpicker-solarized.rb
@@ -0,0 +1,9 @@
+cask "colorpicker-solarized" do
+  version "v1.0.0beta2"
+  sha256 :no_check
+  
+  url "https://ethanschoonover.com/solarized/files/solarized.zip"
+  name "Solarized colorpicker"
+  homepage "https://ethanschoonover.com/solarized/"
+  colorpicker "solarized/apple-colorpalette-solarized/Solarized.clr"
+end
