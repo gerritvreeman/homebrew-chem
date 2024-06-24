871330c8d5dd6bf9e2b4f1bb815e2ad9916450a1
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-24 10:27:44 -0500

    Create colorpicker-nuco.rb

---
+++ Casks/colorpicker-nuco.rb
@@ -0,0 +1,9 @@
+cask "colorpicker-nuco" do
+  version "1.0.0"
+  sha256 :no_check
+  
+  url "https://github.com/gerritvreeman/nuco-colors/releases/download/1.0.0/NuCo.clr.zip"
+  name "NuCo colorpicker"
+  homepage "https://github.com/gerritvreeman/nuco-colors/"
+  colorpicker "NuCo.clr"
+end
