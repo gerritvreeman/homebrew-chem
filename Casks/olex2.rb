8b58d54b3d05fac3219a3a71e314346bf9d4d821
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-24 00:06:53 -0500

    Update olex2.rb
    add verified

--- Casks/olex2.rb
+++ Casks/olex2.rb
@@ -8,8 +8,8 @@ cask "olex2" do
   version "1.5"
   sha256 "40ee824f3acd4fc258328d196f8ca35f3e0c0eb5168d86b0c188f788242a8470"
   url "https://secure.olex2.org/olex2-distro/olex2-#{version}.app.dmg",
-      :using => MyDownloadStrategy
-      #verified: "secure.olex2.org/"
+      :using => MyDownloadStrategy,
+      verified: "secure.olex2.org/"
   name "olex2"
   desc "An easy-to-use program containing everything you need to solve, refine and finish small-molecule crystal structures."
   homepage "https://www.olexsys.org/olex2/"
