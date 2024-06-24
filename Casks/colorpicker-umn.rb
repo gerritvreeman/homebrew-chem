695a3f40e8a6524014fd017e623b35ae5d60a697
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-06-24 10:21:39 -0500

    Create colorpicker-umn.rb

---
+++ Casks/colorpicker-umn.rb
@@ -0,0 +1,9 @@
+cask "colorpicker-umn" do
+  version "1.0.0"
+  sha256 :no_check
+  
+  url "https://github.com/gerritvreeman/umn-colors/releases/download/1.0.0/UMN.clr.zip"
+  name "UMN colorpicker"
+  homepage "https://github.com/gerritvreeman/umn-colors/"
+  colorpicker "UMN.clr"
+end
